80700cc9dab0e2fc9e788ee966f835bcade977de
JideOgun <JideOgun@github.com>
2022-02-09 13:19:26 -0600

    set up connection to server, created tables with columns and set up api route for categories

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll().then(dbProductData => {
+    res.json(dbProductData)
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  
 });
 
 // create new product
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const categoryRoutes = require('./api/category-routes');
 
 router.use('/api', apiRoutes);
+router.use('/categories', categoryRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+// router.use((req, res) => {
+//   res.send("<h1>Wrong Route!</h1>")
+// });
 
 module.exports = router;
